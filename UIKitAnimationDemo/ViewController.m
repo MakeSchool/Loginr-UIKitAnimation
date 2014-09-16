badcf1a06d1d2b356c879b29455fbe07108e642a
Daniel Haaser <thedapperdan@gmail.com>
2014-09-16 16:41:43 -0700

    Added root view with fancy animations

--- UIKitAnimationDemo/ViewController.m
+++ UIKitAnimationDemo/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+@property (weak, nonatomic) IBOutlet UIButton *btnSignIn;
+@property (weak, nonatomic) IBOutlet UIButton *btnCreateAccount;
+
 @end
 
 @implementation ViewController
@@ -22,6 +26,61 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self doSomeFancyBlockBasedAnimations];
+}
+
+- (void)doSomeFancyBlockBasedAnimations {
+    
+    // We're going to move the title and the two buttons off the screen and animate them in
+    // We're also going to do an alpha fade for good measure
+    
+    // Save the original positions of the elements
+    CGRect originalTitleFrame = self.lblTitle.frame;
+    CGRect originalSignInFrame = self.btnSignIn.frame;
+    CGRect originalCreateAccountFrame = self.btnCreateAccount.frame;
+    
+    // Create the offscreen positions based on their original positions
+    CGRect offscreenTitleFrame = CGRectMake(originalTitleFrame.origin.x,
+                                            originalTitleFrame.origin.y - self.view.frame.size.height / 2.0f,
+                                            originalTitleFrame.size.width,
+                                            originalTitleFrame.size.height);
+    
+    CGRect offscreenSignInFrame = CGRectMake(originalSignInFrame.origin.x + self.view.frame.size.width,
+                                             originalSignInFrame.origin.y,
+                                             originalSignInFrame.size.width,
+                                             originalSignInFrame.size.height);
+    
+    CGRect offscreenCreateAccountFrame = CGRectMake(originalCreateAccountFrame.origin.x - self.view.frame.size.width,
+                                                    originalCreateAccountFrame.origin.y,
+                                                    originalCreateAccountFrame.size.width,
+                                                    originalCreateAccountFrame.size.height);
+    
+    // Set the UI elements to their pre-animation state
+    self.lblTitle.alpha = 0.0f;
+    self.btnSignIn.alpha = 0.0f;
+    self.btnCreateAccount.alpha = 0.0f;
+    
+    self.lblTitle.frame = offscreenTitleFrame;
+    self.btnSignIn.frame = offscreenSignInFrame;
+    self.btnCreateAccount.frame = offscreenCreateAccountFrame;
+    
+    [UIView animateWithDuration:1.5f delay:0.0f usingSpringWithDamping:0.75f initialSpringVelocity:1.0f options:UIViewAnimationOptionCurveEaseIn animations:^{
+        self.lblTitle.alpha = 1.0f;
+        self.btnSignIn.alpha = 1.0f;
+        self.btnCreateAccount.alpha = 1.0f;
+        
+        self.lblTitle.frame = originalTitleFrame;
+        self.btnSignIn.frame = originalSignInFrame;
+        self.btnCreateAccount.frame = originalCreateAccountFrame;
+    } completion:^(BOOL finished) {
+        NSLog(@"Fancy animation complete!");
+    }];
 }
 
 @end
