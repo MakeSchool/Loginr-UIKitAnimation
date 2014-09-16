8760ac6a00a766369da9b491eec9791d4a3d72ac
Daniel Haaser <thedapperdan@gmail.com>
2014-09-16 12:42:13 -0700

    Created Project

---
+++ UIKitAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIKitAnimationDemo
+//
+//  Created by Daniel Haaser on 9/16/14.
+//  Copyright (c) 2014 makeschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIKitAnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIKitAnimationDemo
+//
+//  Created by Daniel Haaser on 9/16/14.
+//  Copyright (c) 2014 makeschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
